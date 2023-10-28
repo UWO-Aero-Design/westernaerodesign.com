b3e92bf26f28a0a6e780307e404a12d8bcbf2414
Alex Fernandes <everything.alex04@gmail.com>
2023-10-28 15:53:56 -0400

    added team pictures

--- src/Members.js
+++ src/Members.js
@@ -1,36 +1,56 @@
 import Jeff from './assets/members/jeff.png'
 import Matt from './assets/members/matt.png'
-import Colin from "./assets/members/colin.png"
+import Jamie from "./assets/members/jamie.JPG";
+import Christian from "./assets/members/christian.JPG"
+import Brett from "./assets/members/brett.JPG";
+import Rishi from "./assets/members/rishi.jpg"
+import Henry from "./assets/members/henry.JPG"
+import Shreya from "./assets/members/shreya.JPG";
+import Om from"./assets/members/om.JPG";
+import Ben from "./assets/members/ben.JPG";
+import Andres from "./assets/members/andres.JPG";
+import MatthewB from "./assets/members/matthewB.JPG";
+import Shahmeer from "./assets/members/shahmeer.JPG";
+import Troy from "./assets/members/troy.JPG";
+import Owen from "./assets/members/owen.JPG";
+import MattD from "./assets/members/matt.JPG"
+
+import Colin from "./assets/members/colin.JPG";
+import Khathab from "./assets/members/khathab.JPG";
+import John from "./assets/members/john.JPG";
+import Shiven from "./assets/members/shiven.JPG";
+import Dhruv from "./assets/members/dhruv.JPG";
+import Alex from "./assets/members/alex.JPG"
 import placeholder from "./assets/logo_plane.png";
 
 
 const MechanicalMembers = [
   { name: 'Matthew Soldo', title: 'Mechanical Co-Captain', year: '5th Year Mechanical Engineering w/ Co-Op', image: Matt },
-  { name: 'Jamieson Eng', title: 'Mechanical Co-Captain', year: '5th Year Mechanical Engineering w/ Co-Op ', image: placeholder },
-  { name: 'Christian Seabrook', title: 'Wing Sr. Lead', year: '5th Year Mechanical & Biomedical Engineering', image: placeholder },
-  { name: 'Brett MacDonald', title: 'Fuselage Sr. Lead', year: '5th Year Mechanical Engineering w/ Co-Op', image: placeholder },
-  { name: 'Rishi Patel', title: 'Tail Sr. Lead', year: '4th Year Mechatronics Engineering', image: placeholder },
-  { name: 'Henry Li', title: 'Landing Gear Sr. Lead', year: '3rd Year Mechanical and Biomedical Engineering ', image: placeholder },
-  { name: 'Shreya Chandel', title: 'PADA Mechanical Sr. Lead', year: '5th Year Mechanical Engineering w/ Co-Op ', image: placeholder },
-  { name: 'Om Bhatt', title: 'GTV Sr. Lead', year: '3rd Year Mechatronics Engineering and AISE', image: placeholder },
-  { name: 'Ben Secco', title: 'Wing Jr. Lead', year: '2nd Year Civil Engineering', image: placeholder },
-  { name: 'Andres Monroy', title: 'Fuselage Jr. Lead', year: '4th Year Mechanical Engineering w/ Co-Op ', image: placeholder },
-  { name: 'Matthew Bentley', title: 'Tail Jr. Lead', year: '4th Year Mechanical Engineering w/ Co-Op ', image: placeholder },
-  { name: 'Shahmeer Atif', title: 'Landing Gear Jr. Lead', year: '2nd Year Electrical Engineering', image: placeholder },
-  { name: 'Leon Han', title: 'PADA Mechanical Jr. Lead', year: '2nd Year Mechanical Engineering', image: placeholder },
-  { name: 'Shajith Shajahan', title: 'PADA Mechanical Jr. Lead', year: '2nd Year Software Engineering ', image: placeholder },
-  { name: 'Matt (Le Hoang Anh) Doan', title: 'GTV Jr. Lead', year: '3rd Year Mechatronics Engineering', image: placeholder },
+  { name: 'Jamieson Eng', title: 'Mechanical Co-Captain', year: '5th Year Mechanical Engineering w/ Co-Op ', image: Jamie },
+  { name: 'Christian Seabrook', title: 'Wing Sr. Lead', year: '5th Year Mechanical & Biomedical Engineering', image: Christian },
+  { name: 'Brett MacDonald', title: 'Fuselage Sr. Lead', year: '5th Year Mechanical Engineering w/ Co-Op', image: Brett },
+  { name: 'Rishi Patel', title: 'Tail Sr. Lead', year: '4th Year Mechatronics Engineering', image: Rishi },
+  { name: 'Henry Li', title: 'Landing Gear Sr. Lead', year: '3rd Year Mechanical and Biomedical Engineering ', image: Henry },
+  { name: 'Shreya Chandel', title: 'PADA Mechanical Sr. Lead', year: '5th Year Mechanical Engineering w/ Co-Op ', image: Shreya },
+  { name: 'Om Bhatt', title: 'GTV Sr. Lead', year: '3rd Year Mechatronics Engineering and AISE', image: Om },
+  { name: 'Ben Secco', title: 'Wing Jr. Lead', year: '2nd Year Civil Engineering', image: Ben },
+  { name: 'Andres Monroy', title: 'Fuselage Jr. Lead', year: '4th Year Mechanical Engineering w/ Co-Op ', image: Andres },
+  { name: 'Matthew Bentley', title: 'Tail Jr. Lead', year: '4th Year Mechanical Engineering w/ Co-Op ', image: MatthewB },
+  { name: 'Shahmeer Atif', title: 'Landing Gear Jr. Lead', year: '2nd Year Electrical Engineering', image: Shahmeer },
+  { name: 'Troy Leishman', title: 'PADA Mechanical Jr. Lead', year: '1st Year Engineering', image: Troy },
+  { name: 'Owen Ellis', title: 'PADA Mechanical Jr. Lead', year: '2nd Year Mechatronics Engineering and Astrophysics ', image: Owen },
+  { name: 'Matt (Le Hoang Anh) Doan', title: 'GTV Jr. Lead', year: '3rd Year Mechatronics Engineering', image: MattD },
 
 ]
 
 const ControlsMembers = [
   { name: 'Jeff St. Jean', title: 'Controls Co-Captain', year: '6th Year Mechatronics Engineering w/ Co-Op & CS ', image: Jeff },
   { name: 'Colin Smith', title: 'Controls Co-Captain', year: '5th Year Mechatronics Engineering w/ Co-Op & CS', image: Colin },
-  { name: 'Khathab Hamed', title: 'PADA Controls Sr. Lead', year: '4th Year Mechatronics Engineering & CS', image: placeholder },
-  { name: 'John Langlois', title: 'PA Controls Sr. Lead', year: '4th Year Software Engineering w/ Co-Op ', image: placeholder },
-  { name: 'Shiven Sharma', title: 'PADA Controls Jr. Lead', year: '2nd Year Software Engineering', image: placeholder },
-  { name: 'Dhruv Patel', title: 'Embedded Controls Jr. Lead', year: '4th Year Mechatronics and Biomedical Engineering', image: placeholder },
-  { name: 'Alex Fernandes', title: 'Ground Station Jr. Lead', year: '2nd Year Electrical Engineering & AISE ', image: placeholder },
+  { name: 'Khathab Hamed', title: 'PADA Controls Sr. Lead', year: '4th Year Mechatronics Engineering & CS', image: Khathab },
+  { name: 'John Langlois', title: 'PA Controls Sr. Lead', year: '4th Year Software Engineering w/ Co-Op ', image: John },
+  { name: 'Shiven Sharma', title: 'PADA Controls Jr. Lead', year: '2nd Year Software Engineering', image: Shiven },
+  { name: 'Dhruv Patel', title: 'Embedded Controls Jr. Lead', year: '4th Year Mechatronics and Biomedical Engineering', image: Dhruv },
+  { name: 'Alex Fernandes', title: 'Ground Station Jr. Lead', year: '2nd Year Electrical Engineering & AISE ', image: Alex },
 
 
 ] 
--- src/Support.js
+++ src/Support.js
@@ -16,15 +16,15 @@ import WhyDesign from "./assets/sponsors/WhyDesign.png";
 const Support = () => {
     return (
         <section id="support" className='my-16 md:mx-28 mx-12 mx-auto scroll-m-20'>
-            <h1 className='text-center pb-5 mb-10 border-b-2 font-bold text-4xl'>Industry Sponsors</h1>
+            <h1 className='text-center pb-5 mb-10 border-b-2 font-bold text-4xl'>Donors</h1>
             <div>
-            <h2 className='font-medium text-2xl'>Interested in becoming a sponsor?</h2> 
+            <h2 className='font-medium text-2xl'>Interested in becoming a donor?</h2> 
             <p className='py-3 text-lg'>
-                Feel free to take a look at our <a download="Sponsorship Package" className='font-medium text-purple-700 hover:text-purple-500 transition-all duration-300' href={SponsorshipPackage}> sponsorship package </a> and send us an email at <a className='font-medium text-purple-700 hover:text-purple-500 transition-all duration-300' href="mailto:western.aerodesign.com">western.aerodesign@gmail.com</a>. 
+                Feel free to take a look at our <a download="Sponsorship Package" className='font-medium text-purple-700 hover:text-purple-500 transition-all duration-300' href={SponsorshipPackage}> donation package </a> and send us an email at <a className='font-medium text-purple-700 hover:text-purple-500 transition-all duration-300' href="mailto:western.aerodesign.com">western.aerodesign@gmail.com</a>. 
                 We look forward to working with you!
             </p>
             </div>
-            <h3 className='text-5xl font-extrabold text-center my-4 py-4 text-transparent bg-clip-text bg-gradient-to-br from-pink-500 to-yellow-500 border-b-2 '>GOLD SPONSORS</h3>
+            <h3 className='text-5xl font-extrabold text-center my-4 py-4 text-transparent bg-clip-text bg-gradient-to-br from-pink-500 to-yellow-500 border-b-2 '>GOLD DONORS</h3>
             <div className="grid place-items-center grid-cols-1 lg:grid-cols-3 gap-6">
                 <a href="http://www.armateconline.com/"><img className='my-2 hover:-translate-y-1 hover:scale-110 duration-300' src={Armatec} alt='Armatec'></img>
                 <figcaption className="pt-4 text-center text-gray-800 italic font-semibold">
@@ -41,7 +41,7 @@ const Support = () => {
                     For the Passion of Flying
                 </figcaption></a>
             </div>
-            <h3 className='text-4xl font-bold text-center my-4 py-4 text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-silver-500 border-b-2 '>SILVER SPONSORS</h3>
+            <h3 className='text-4xl font-bold text-center my-4 py-4 text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-silver-500 border-b-2 '>SILVER DONORS</h3>
             <div className="grid place-items-center grid-cols-2 lg:grid-cols-4 gap-5">
                 <a href="https://www.siemens.com/ca/en.html"><img className='my-2 hover:-translate-y-1 hover:scale-110 duration-300' src={Siemens} alt='Siemens'></img></a>
                 <a href="https://lmac.ca/"><img className='my-2 px-12 hover:-translate-y-1 hover:scale-110 duration-300' src={RC} alt='London Model Aircraft Club'></img></a>
@@ -50,7 +50,7 @@ const Support = () => {
                 <a href="https://trackunit.com/"><img className='my-2 hover:-translate-y-1 hover:scale-110 duration-300' src={TrackUnit} alt='TrackUnit'></img></a>
                 <a href="https://whydesign.ca/"><img className='my-2 hover:-translate-y-1 hover:scale-110 duration-300' src={WhyDesign} alt='WhyDesign'></img></a>
             </div>
-            <h3 className='text-3xl font-bold text-center my-4 py-4  text-transparent bg-clip-text bg-gradient-to-br from-gray-500 to-orange-500 border-b-2 '>BRONZE SPONSORS</h3>
+            <h3 className='text-3xl font-bold text-center my-4 py-4  text-transparent bg-clip-text bg-gradient-to-br from-gray-500 to-orange-500 border-b-2 '>BRONZE DONORS</h3>
             <div className="grid place-items-center grid-cols-2 lg:grid-cols-5 gap-5">
                 <a href="https://www.protocase.com/"><img className='my-2 md:px-10 px-4 hover:-translate-y-1 hover:scale-110 duration-300' src={Protocase} alt='Protocase'></img></a>
             </div>
